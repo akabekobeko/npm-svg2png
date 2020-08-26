2e1c33825059a46d1bea01ef1b79e3578b30b3b6
akabeko <akabekobeko@gmail.com>
2020-08-26 18:12:40 +0900

    #9 Add an examples

---
+++ examples/README.md
@@ -0,0 +1,29 @@
+# Examples for @akabeko/svg2png
+
+[![license: MIT](https://img.shields.io/badge/license-MIT-blue)](https://github.com/akabekobeko/npm-svg2png/blob/master/LICENSE)
+
+## Installation
+
+```
+$ npm install
+```
+
+## Usage
+
+### Node.js API
+
+`js` is JavaScript. `ts` is TypeScript on `ts-node`.
+
+```
+$ npm run js
+$ npm run ts
+```
+
+### CLI
+
+`cli:single` produces a single file, `cli:multiple` produces multiple files.
+
+```
+$ npm run cli:single
+$ npm run cli:multiple
+```
---
+++ examples/index.js
@@ -0,0 +1,22 @@
+const svg2png = require('@akabeko/svg2png').svg2png
+
+svg2png({
+  input: './sample.svg',
+  output: './sample.png',
+  sizes: [
+    { width: 256, height: 256 },
+    { width: 128, height: 256 }
+  ],
+  //executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  fetcher: {
+    revision: '782078',
+    path: './renderer'
+  }
+})
+  .then((results) => {
+    console.log(results)
+    console.log('Completed!!!')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
